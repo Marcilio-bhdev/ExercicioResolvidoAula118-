6e5bae5f2d9f12e09ecf7e2a0ad629587a93947d
Marcilio-bhdev <marcilio.santos@meta3.com.br>
2020-02-18 13:39:17 -0300

    Terceiro commite do projeto

--- ExercicioResolvidoAula118/Entities/Worker.cs
+++ ExercicioResolvidoAula118/Entities/Worker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ExercicioResolvidoAula118.Entities.Enums;
 
 namespace ExercicioResolvidoAula118.Entities
@@ -9,5 +10,46 @@ namespace ExercicioResolvidoAula118.Entities
         public WorkerLevel  Level { get; set; }//esta propriedade e exclusiva criada da classe "enums"
         public double BaseSalary { get; set; }
         public Departament Departament { get; set; }//aqui criei uma Associação de classes (composição de objetos) chamando o objeto Departament no objeto Worker
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();/*Aqui foi criado uma lista como propriedade associando a classe contratos a classe worker*/
+
+        public Worker() 
+        {
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)// criei este construtor sem a lista
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddContract(HourContract contract) /*Aqui criei um metodo para adicionar contratos pegando por referencia a variável contract*/
+        {
+            Contracts.Add(contract);//aqui coloquei a variavel contract como referencia
+            
+        
+        }
+        public void RemoveContract(HourContract contract)
+        {
+
+            Contracts.Remove(contract);
+        
+        }
+
+        public double Income(int year, int month) 
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts) //aqui usei um foreach para percorrer todos os contratos
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)//aqui determinei que se ele os contratos estiveram com mesmo mes e ano que os registrados na lista
+                {
+                    sum += contract.TotalValue();
+                
+                }
+            }
+            return sum;
+        }
     }
+
 }
