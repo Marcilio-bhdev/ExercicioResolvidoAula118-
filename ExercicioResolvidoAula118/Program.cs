164b6aae2842244253f72135496d032a530ca4fd
Marcilio-bhdev <marcilio.santos@meta3.com.br>
2020-02-18 15:46:15 -0300

    Ultimo commite do projeto, que esta todo comentado

--- ExercicioResolvidoAula118/Program.cs
+++ ExercicioResolvidoAula118/Program.cs
@@ -24,6 +24,34 @@ namespace ExercicioResolvidoAula118
             Departament dept = new Departament(deptName);
             Worker worker = new Worker(name, level, baseSalary, dept);
 
+            Console.Write("How many contracts to this Worker?: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value Per Hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (Hour): ");
+                int hour = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hour);// aqui fiz a intanciação da classe contrato
+                worker.AddContract(contract);//aqui eu adicionei os contratos por trabalhador
+
+            }
+            Console.WriteLine();
+            Console.Write("Enter month and year to alculate income(MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));//Aqui Usei o substring para recortar o mes
+            int year = int.Parse(monthAndYear.Substring(3));//Aqui Usei o substring para recortar o ano
+            Console.WriteLine("Name: "+ worker.Name);
+            Console.WriteLine("Departament: " + worker.Departament.Name);
+            Console.WriteLine("Income for"+month+year+": "+ worker.Income(year,month).ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
 
 
 
