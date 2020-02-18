4a17591aeb9e5369fc2058a10a58b25d0cd295dd
Marcilio-bhdev <marcilio.santos@meta3.com.br>
2020-02-18 10:39:35 -0300

    Projeto Iniciado Primeira parte

---
+++ ExercicioResolvidoAula118/Entities/Departament.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioResolvidoAula118.Entities
+{
+    class Departament
+    {
+    }
+}
